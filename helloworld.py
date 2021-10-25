6f736f7305c7401175b01259a51c69c1433192e3
hsuandy <andyhsu@gmail.com>
2021-10-24 17:44:21 -0700

    Merge clean up

--- helloworld.py
+++ helloworld.py
@@ -5,12 +5,3 @@ def main():
     hello_world('NYC', 'New York')
 
 main()
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-def main():
-    hello_world('NYC')
->>>>>>> refs/remotes/origin/main
-
-main()
